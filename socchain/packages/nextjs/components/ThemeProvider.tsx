decafafbbcf7d8d86bbb9e660df7c4b8538af4ff
Emmanuel Nyamweya <ogoti.nyamweya@strathmore.edu>
2025-11-23 18:59:00 +0300

    Update ThemeProvider.tsx

--- socchain/packages/nextjs/components/ThemeProvider.tsx
+++ socchain/packages/nextjs/components/ThemeProvider.tsx
@@ -1,9 +1,21 @@
+// components/ThemeProvider.tsx
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { useEffect, useState } from "react";
 
 export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Prevent next-themes from running before full client mount
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 };
